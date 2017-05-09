c0906d8c07382c9e62d5f3edd82e764f87d08419
Janek Brandt <brandt.janek@gmail.com>
2017-05-08 18:27:48 -0700

    fix refresh error by adding safe navigation operator to the property path in the template

--- src/app/listing-details/listing-details.component.ts
+++ src/app/listing-details/listing-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { Listing } from '../listing.model';
@@ -9,7 +9,7 @@ import { ListingService } from '../listing.service';
   templateUrl: './listing-details.component.html',
   styleUrls: ['./listing-details.component.css']
 })
-export class ListingDetailsComponent implements OnInit, OnChanges {
+export class ListingDetailsComponent implements OnInit {
   listingKey: string = null;
   categoryId: string = null;
   listing: Listing;
@@ -17,32 +17,12 @@ export class ListingDetailsComponent implements OnInit, OnChanges {
   constructor(private route: ActivatedRoute, private location: Location, private listingService: ListingService, private router: Router) { }
 
   ngOnInit() {
-    this.getData();
-    // this.route.params.forEach((urlParameters) => {
-    //   this.categoryId = urlParameters['categoryId'];
-    //   this.listingKey = urlParameters['listingId'];
-    // });
-    // this.listingService.getListing(this.listingKey).subscribe((listing) => {
-    //   this.listing = listing;
-    //   console.log("at end",this.listing)
-    //
-    // });
-  }
-
-  ngOnChanges() {
-    this.getData();
-  }
-
-  getData() {
-    console.log(this.listing)
     this.route.params.forEach((urlParameters) => {
       this.categoryId = urlParameters['categoryId'];
       this.listingKey = urlParameters['listingId'];
     });
     this.listingService.getListing(this.listingKey).subscribe((listing) => {
       this.listing = listing;
-      console.log("at end",this.listing)
-
     });
   }
 
